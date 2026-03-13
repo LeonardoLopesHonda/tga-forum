f2b9ad14cdb4789c1c9ada5e449307c3563b33dc
leonardo honda <leozinhohondams@gmail.com>
2026-03-13 17:27:33 -0400

    feat: write simple routes and mock payloads

--- main.py
+++ main.py
@@ -2,10 +2,203 @@ from fastapi import FastAPI # type: ignore
 
 app = FastAPI()
 
-users = []
-posts = []
-comments = []
+# AUTH
+@app.post("/auth")
+def create_session(email: str, password: str):
+    return {"access_token": "mock_token_123"}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# USERS
+@app.post("/users")
+def create_user(username: str, email: str, password: str):
+    return { 
+        "user_id": 1,
+        "username": "user1",
+        "email": "user1.mail@gmail.com",
+    }
+
+@app.put("/users/{user_id}")
+def update_user(access_token: str, username: str, email: str, password: str):
+    return { 
+        "user_id": 1,
+        "username": "user1",
+        "email": "user1.mail@gmail.com",
+    }
+
+@app.delete("/users/{user_id}")
+def delete_user(access_token: str):
+    return {"message": "User deleted"}
+
+@app.get("/users")
+def get_users(access_token: str):
+    return {[
+        {
+            "username": "user1",
+            "email": "user1.mail@gmail.com",
+        }, {
+            "username": "user2",
+            "email": "user2.mail@gmail.com",
+        }
+    ]}
+
+@app.get("/users/{user_id}")
+def get_user(access_token: str):
+    return {
+        "username": "user1",
+        "email": "user1.mail@gmail.com",
+    }
+
+@app.get("/users/{user_id}/posts")
+def get_user(access_token: str):
+    return {
+        "username": "user1",
+        "posts": [
+            "post1",
+            "post2"
+        ],
+    }
+
+@app.get("/users/{user_id}/comments")
+def get_user(access_token: str):
+    return {
+        "username": "user1",
+        "comments": [
+            "comment1",
+            "comment2"
+        ],
+    }
+
+# POSTS
+@app.post("/posts")
+def create_post(access_token: str, title: str, content: str):
+    return { 
+        "post_id": 1,
+        "user_id": 1,
+        "title": "Post's title",
+        "content": "Post's content",
+    }
+
+@app.put("/posts/{post_id}")
+def update_post(access_token: str, title: str, content: str):
+    return { 
+        "post_id": 1,
+        "user_id": 1,
+        "title": "Post's title",
+        "content": "Post's content",
+    }
+
+@app.delete("/posts/{post_id}")
+def delete_post(access_token: str, post_id: id):
+    return {"message": "Post deleted"}
+
+@app.get("/posts")
+def get_posts(access_token: str):
+    return {[
+        {
+            "user_id": 1,
+            "post_id": "post1",
+            "title": "Post1 title",
+            "content": "Post1 content",
+        }, {
+            "user_id": 2,
+            "post_id": "post2",
+            "title": "Post2 title",
+            "content": "Post2 content",
+        }
+    ]}
+
+@app.get("/posts/{post_id}")
+def get_post(access_token: str):
+    return {
+        "user_id": 2,
+        "post_id": "post2",
+        "title": "Post2 title",
+        "content": "Post2 content",
+    }
+
+# Comments
+@app.post("/posts/comments/{post_id}")
+def create_comment(access_token: str, post_id: int, content: str, parent_id: int | None = None):
+    return { 
+        "comment_id": 1,
+        "parent_id": None,
+        "post_id": 1,
+        "user_id": 1,
+        "content": "Comments's content",
+    }
+
+@app.post("/posts/replies/{parentComment_id}")
+def create_reply(access_token: str, parent_id: int, post_id: int, user_id: int, title: str, content: str):
+    return { 
+        "comment_id": 2,
+        "parent_id": 1,
+        "post_id": 1,
+        "user_id": 1,
+        "content": "Reply's content",
+    }
+
+@app.put("/posts/comments/{comment_id}")
+def update_comment(access_token: str, comment_id: int, title: str, content: str):
+    return { 
+        "comment_id": 1,
+        "parent_id": None,
+        "post_id": 1,
+        "user_id": 1,
+        "content": "Reply's content",
+    }
+
+@app.delete("/posts/comments/{comment_id}")
+def delete_comment(access_token: str, comment_id: id):
+    return {"message": "Comment deleted"}
+
+@app.get("/posts/comments/{post_id}")
+def get_comments(access_token: str, comment_id: int):
+    return {[ 
+        {
+            "comment_id": 1,
+            "parent_id": None,
+            "post_id": 1,
+            "user_id": 1,
+            "content": "Comments's content",
+        },
+        {
+            "comment_id": 2,
+            "parent_id": 1,
+            "post_id": 1,
+            "user_id": 1,
+            "content": "Reply's content",
+        },
+    ]}
+
+@app.get("/posts/comments/{comment_id}")
+def get_comment(access_token: str, comment_id: int, title: str, content: str):
+    return { 
+        "comment_id": 1,
+        "parent_id": None,
+        "post_id": 1,
+        "user_id": 1,
+        "content": "Comments's content",
+    }
+
+@app.get("/posts/comments/{comment_id}/ancestors")
+def get_comment(access_token: str, comment_id: int, title: str, content: str):
+    return {[ 
+        {
+            "comment_id": 1,
+            "parent_id": None,
+            "post_id": 1,
+            "user_id": 1,
+            "content": "Comments's content",
+        }
+    ]}
+
+@app.get("/posts/comments/{comment_id}/breadcrumb")
+def get_comment(access_token: str, comment_id: int, title: str, content: str):
+    return {[ 
+        {
+            "comment_id": 2,
+            "parent_id": 1,
+            "post_id": 1,
+            "user_id": 1,
+            "content": "Reply's content",
+        },
+    ]}
