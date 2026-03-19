ea3a8f9ea64e8c89cc22ecff2d6a7807f6720179
leonardo honda <leozinhohondams@gmail.com>
2026-03-19 17:20:39 -0400

    feat: add function to generate tables

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,10 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship, mapped_column, sessionmaker
+from sqlalchemy import ForeignKey
+from sqlalchemy import create_engine
 from typing import List
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, relationship, mapped_column
 
-engine = create_engine("postgres://postgres:postgres123@localhost:5432/tga-forum", echo=True)
+engine = create_engine("postgresql+psycopg2://postgres:postgres123@localhost:5432/tga-forum", echo=True)
+session = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
@@ -26,8 +28,8 @@ class Post(Base):
     content: Mapped[str] = mapped_column(nullable=False)
 
     user_id: Mapped[int] = mapped_column(ForeignKey("user.user_id"))
-    user: Mapped["User"] = relationship(back_populates="post")
-    comments: Mapped[List["Comment"]] = relationship(back_populates="post")
+    user: Mapped["User"] = relationship(back_populates="posts")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="posts")
 
 class Comment(Base):
     __tablename__ = "comment"
@@ -35,9 +37,19 @@ class Comment(Base):
     comment_id: Mapped[int] = mapped_column(primary_key=True)
     content: Mapped[str] = mapped_column(nullable=False)
 
-    parent_id: Mapped[int] = mapped_column(ForeignKey("post.post_id"), nullable=True)
+    parent_id: Mapped[int] = mapped_column(ForeignKey("comment.comment_id"), nullable=True)
     comment: Mapped["Comment"] = relationship(back_populates="comment")
     post_id: Mapped[int] = mapped_column(ForeignKey("post.post_id"))
-    post: Mapped["Post"] = relationship(back_populates="comment")
+    post: Mapped["Post"] = relationship(back_populates="comments")
     user_id: Mapped[int] = mapped_column(ForeignKey("user.user_id"))
-    user: Mapped["User"] = relationship(back_populates="comment")
+    user: Mapped["User"] = relationship(back_populates="comments")
+
+def create_tables():
+    Base.metadata.create_all(engine)
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
