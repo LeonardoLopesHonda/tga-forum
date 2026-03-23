71940a94577a6918b86555268903a64fd7cfc584
leonardo honda <leozinhohondams@gmail.com>
2026-03-22 22:11:20 -0400

    feat: add `PEPPER` to env configuration

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,8 +2,9 @@ from pathlib import Path
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    JWT_SECRET_KEY: str
     DATABASE_URL: str
+    JWT_SECRET_KEY: str
+    PEPPER: str
 
     model_config = { "env_file": Path(__file__).resolve().parent.parent.parent / ".env" }
 
