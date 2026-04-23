5ef267e1662056e0bc7638bed83309a8dd99867c
leonardo honda <leozinhohondams@gmail.com>
2026-04-23 05:53:20 -0400

    feat: add auth test suite

---
+++ backend/app/tests/test_auth.py
@@ -0,0 +1,84 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import StaticPool, create_engine
+from sqlalchemy.orm import sessionmaker
+
+from db.database import Base, get_db
+from main import app
+
+engine = create_engine(
+    "sqlite:///:memory:",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSession = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def override_get_db():
+    db = TestingSession()
+    try:
+        yield db
+    finally:
+        db.close()
+
+client = TestClient(app)
+
+@pytest.fixture(autouse=True)
+def setup_db():
+    Base.metadata.create_all(bind=engine)
+    app.dependency_overrides[get_db] = override_get_db
+    yield
+    Base.metadata.drop_all(bind=engine)
+    app.dependency_overrides.clear()
+
+def test_user_authentication():
+    response = client.post("/api/v1/user", json={
+        "username": "test_read",
+        "email": "test_read@gmail.com",
+        "password": "12345678",
+    })
+    assert response.status_code == 200, response.text
+
+    response = client.post("/api/v1/auth", data={
+        "username": "test_read@gmail.com",
+        "password": "12345678",
+    })
+
+    data = response.json()
+    assert data["access_token"]
+    assert data["token_type"] == "bearer"
+
+def test_user_authorized_request():
+    response = client.post("/api/v1/user", json={
+        "username": "authorized",
+        "email": "authorized@gmail.com",
+        "password": "12345678",
+    })
+    assert response.status_code == 200, response.text
+    user = response.json()
+    user_id = user["user_id"]
+
+    response = client.post("/api/v1/auth", data={
+        "username": "authorized@gmail.com",
+        "password": "12345678",
+    })
+    data = response.json()
+    access_token = data["access_token"]
+
+    response = client.get(f"/api/v1/user/{user_id}", headers={"Authorization": f"Bearer {access_token}"})
+    data = response.json()
+    assert response.status_code == 200, response.text
+    assert data["username"] == "authorized"
+    assert data["email"] == "authorized@gmail.com"
+
+def test_user_unauthorized_request():
+    response = client.post("/api/v1/user", json={
+        "username": "unauthorized",
+        "email": "unauthorized@gmail.com",
+        "password": "12345678",
+    })
+    assert response.status_code == 200, response.text
+
+    user = response.json()
+
+    response = client.get(f"/api/v1/user/{user["user_id"]}")
+    assert response.status_code == 401, response.text
