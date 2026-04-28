9dd2e6961074f347903ae028a93f4047793e6edb
leonardo honda <leozinhohondams@gmail.com>
2026-04-28 09:09:15 -0400

    feat: add vercel url to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ create_tables()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://tga-forum.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
